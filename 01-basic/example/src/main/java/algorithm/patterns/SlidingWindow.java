f7e8639606eb10299f894dc4773f16e0141cf950
rtg <rtg1539@naver.com>
2024-05-15 18:00:27 +0900

    SlidingWindow 패턴

---
+++ 01-basic/example/src/main/java/algorithm/patterns/SlidingWindow.java
@@ -0,0 +1,28 @@
+package algorithm.patterns;
+
+/**
+ * 배열이나 문자열같은 데이터를 입력하거나
+ * 연속적인 해당 데이터의 하위 집합을 찾는 경우 유용
+ * */
+public class SlidingWindow {
+    // 연속된 합이 가장 큰 수를 반환 하는 함수
+    public static Integer maxSubArraySum(int[] arr, int num) {
+        if (arr.length < num) return null;
+
+        int maxSum = 0;
+        int tempSum = 0;
+
+        for (int i = 0; i < num; i++) {
+            maxSum += arr[i];
+        }
+
+        tempSum = maxSum;
+
+        for (int i = num; i < arr.length; i++) {
+            tempSum = tempSum - arr[i - num] + arr[i];
+            maxSum = Math.max(maxSum, tempSum);
+        }
+
+        return maxSum;
+    }
+}
---
+++ 01-basic/example/src/test/java/algorithm/patterns/SlidingWindowTest.java
@@ -0,0 +1,18 @@
+package algorithm.patterns;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.assertj.core.api.Assertions.*;
+import static algorithm.patterns.SlidingWindow.*;
+
+class SlidingWindowTest {
+    @Test
+    public void basicTest() {
+        Integer result = maxSubArraySum(new int[] {1, 4, 3, 5, 5, 5, 9, 1}, 2);
+
+        Assertions.assertThat(result).isEqualTo(14);
+
+    }
+}
