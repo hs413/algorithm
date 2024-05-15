d86fdae4a15cc5f18ae4608c69ab9972b417ba97
rtg <rtg1539@naver.com>
2024-05-15 20:51:53 +0900

    재귀 패턴 개념

---
+++ 01-basic/example/src/main/java/algorithm/recursion/Recursion.java
@@ -0,0 +1,31 @@
+package algorithm.recursion;
+
+/**
+ * 메서드(함수)가 자기 자신을 호출하는 것
+ * - 종료 조건이 없거나 잘못되는 경우 주의
+ * */
+public class Recursion {
+
+    // 반복문을 사용한 factorial
+    public static int factorialFor(int num) {
+        int total = 1;
+        for (int i = num; i > 0; i--) {
+            total *= i;
+        }
+        return total;
+    }
+
+    public static int factorial(int num) {
+        if (num == 1) return 1;
+        return num * factorial(num - 1);
+    }
+
+    /**
+     * power 함수 작성
+     * Math.pow()의 기능을 모방
+     * */
+    public static int power(int num, int pow) {
+        if (pow == 0) return 1;
+        return num * power(num, pow - 1);
+    }
+}
---
+++ 01-basic/example/src/test/java/algorithm/recursion/RecursionTest.java
@@ -0,0 +1,28 @@
+package algorithm.recursion;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import static algorithm.recursion.Recursion.*;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.*;
+
+class RecursionTest {
+    @Test
+    public void factorialTest() {
+        assertThat(factorial(4)).isEqualTo(24);
+        assertThat(factorial(5)).isEqualTo(120);
+    }
+
+    @Test
+    public void powerTest() {
+        assertThat(power(2, 0)).isEqualTo(1);
+        assertThat(power(2, 2)).isEqualTo(4);
+        assertThat(power(2, 4)).isEqualTo(16);
+    }
+
+
+// power(2,0) // 1
+// power(2,2) // 4
+// power(2,4) // 16
+}
