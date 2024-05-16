b62d5d44c64672841a34ca99d01117bc076ad317
rtg <rtg1539@naver.com>
2024-05-16 17:33:14 +0900

    단일 연결 리스트 - pop, shift, unshift

--- 03-data-structure/src/main/java/list/SinglyLinkedList.java
+++ 03-data-structure/src/main/java/list/SinglyLinkedList.java
@@ -15,7 +15,7 @@ public class SinglyLinkedList<T> {
         this.length = length;
     }
 
-    public SinglyLinkedList<T> push(T val){
+    public SinglyLinkedList<T> push(T val) {
         Node<T> newNode = new Node<T>(val);
         if (this.head == null) {
             this.head = newNode;
@@ -23,6 +23,64 @@ public class SinglyLinkedList<T> {
         } else {
             this.tail.next = newNode;
             this.tail = newNode;
+        }
+        this.length++;
+        return this;
+    }
+
+//    public void traverse() {
+//        Node<T> current = this.head;
+//        while(current != null) {
+//            System.out.println(current.val);
+//            current = current.next;
+//        }
+//    }
+
+    public T pop() {
+        if (this.head == null) return null;
+
+        Node<T> current = this.head;
+        Node<T> newTail = current;
+
+        while (current.next != null) {
+            newTail = current;
+            current = current.next;
+        }
+
+        this.tail = newTail;
+        this.tail.next = null;
+        this.length--;
+
+        if (this.length == 0) {
+            this.head = null;
+            this.tail = null;
+        }
+
+        return current.val;
+    }
+
+    public T shift() {
+        if (this.head == null) return null;
+
+        Node<T> currentHead = this.head;
+        this.head = currentHead.next;
+        this.length--;
+
+        if (this.length == 0) {
+            this.tail = null;
+        }
+        return currentHead.val;
+    }
+
+    public SinglyLinkedList<T> unshift(T val) {
+        Node<T> newNode = new Node<T>(val);
+
+        if (this.head == null) {
+            this.head = newNode;
+            this.tail = this.head;
+        } else {
+            newNode.next = this.head;
+            this.head = newNode;
         }
         this.length++;
         return this;
--- 03-data-structure/src/test/java/list/SinglyLinkedListTest.java
+++ 03-data-structure/src/test/java/list/SinglyLinkedListTest.java
@@ -1,16 +1,24 @@
 package list;
 
 import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
 
 class SinglyLinkedListTest {
-    @Test
-    public void test() {
-        SinglyLinkedList<Integer> list = new SinglyLinkedList<>();
+    SinglyLinkedList<Integer> list;
 
+    @BeforeEach
+    public void before() {
+        this.list = new SinglyLinkedList<>();
+
+    }
+
+    @Test
+    public void pushTest() {
         list.push(1);
         list.push(33);
 
@@ -19,5 +27,49 @@ class SinglyLinkedListTest {
         assertThat(list.head.next.val).isEqualTo(33);
         assertThat(list.tail.val).isEqualTo(33);
 
+    }
+//    @Test
+//    public void traverseTest() {
+//        SinglyLinkedList<Integer> list = new SinglyLinkedList<>();
+//        list.push(1);
+//        list.push(33);
+//        list.traverse();
+//    }
+
+    @Test
+    public void popTest() {
+        assertThat(list.pop()).isNull();
+
+        list.push(1);
+        list.push(33);
+
+        assertThat(list.pop()).isEqualTo(33);
+        assertThat(list.length).isEqualTo(1);
+        assertThat(list.pop()).isEqualTo(1);
+        assertThat(list.pop()).isNull();
+    }
+
+    @Test
+    public void shiftTest() {
+        assertThat(list.shift()).isNull();
+
+        list.push(1);
+        list.push(33);
+
+        assertThat(list.shift()).isEqualTo(1);
+        assertThat(list.length).isEqualTo(1);
+        assertThat(list.shift()).isEqualTo(33);
+        assertThat(list.shift()).isNull();
+    }
+
+    @Test
+    public void unshiftTest() {
+        list.unshift(1);
+        list.unshift(33);
+
+        assertThat(list.length).isEqualTo(2);
+        assertThat(list.head.val).isEqualTo(33);
+        assertThat(list.head.next.val).isEqualTo(1);
+        assertThat(list.tail.val).isEqualTo(1);
     }
 }
