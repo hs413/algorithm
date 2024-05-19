b9b7fa0a3ad54afeef9edc53e41eafad88b09702
rtg <rtg1539@naver.com>
2024-05-19 15:52:28 +0900

    동적 프로그래밍 메모이제이션

--- 03-data-structure/src/main/java/dp/DynamicPrograming.java
+++ 03-data-structure/src/main/java/dp/DynamicPrograming.java
@@ -9,4 +9,31 @@ public class DynamicPrograming {
         if (n <= 2) return 1;
         return fib(n - 1) + fib(n - 2);
     }
+
+
+    /**
+     * Memoized을 활용한 동적 프로그래밍 피보나치 함수
+     * O(n)
+     * */
+    public static int fibMemoized(int n) {
+//        Map<Integer, Integer> map = new HashMap<>();
+        Integer[] map = new Integer[n + 1];
+        fib2(map, n);
+
+//        return map.get(n);
+        return map[n];
+    }
+
+//    private static int fib2(Map<Integer, Integer> map, int n) {
+    private static int fib2(Integer[] map, int n) {
+//        if (map.containsKey(n)) return map.get(n);
+        if (map[n] != null) return map[n];
+        if (n <= 2) return 1;
+
+        int result = fib2(map, n - 1) + fib2(map, n - 2);
+//        map.put(n, result);
+        map[n] = result;
+        return result;
+    }
+
 }
--- 03-data-structure/src/test/java/dp/DynamicProgramingTest.java
+++ 03-data-structure/src/test/java/dp/DynamicProgramingTest.java
@@ -12,5 +12,18 @@ class DynamicProgramingTest {
         assertThat(DynamicPrograming.fib(5)).isEqualTo(5);
         assertThat(DynamicPrograming.fib(6)).isEqualTo(8);
         assertThat(DynamicPrograming.fib(7)).isEqualTo(13);
+
+        // 시간이 너무 오래 걸림, 사실상 불가능
+//        DynamicPrograming.fibMemoized(100);
+    }
+
+    @Test
+    public void fibMemoized() {
+        assertThat(DynamicPrograming.fibMemoized(4)).isEqualTo(3);
+        assertThat(DynamicPrograming.fibMemoized(5)).isEqualTo(5);
+        assertThat(DynamicPrograming.fibMemoized(6)).isEqualTo(8);
+        assertThat(DynamicPrograming.fibMemoized(7)).isEqualTo(13);
+
+        DynamicPrograming.fibMemoized(100);
     }
 }
