884fc0efedeff53404bec6ef2dd8d2f04c22a876
rtg <rtg1539@naver.com>
2024-05-19 19:37:29 +0900

    Dijkstra Algorithm 리팩터링

--- 03-data-structure/src/main/java/graph/DijkstraAlgorithm.java
+++ 03-data-structure/src/main/java/graph/DijkstraAlgorithm.java
@@ -2,93 +2,98 @@ package graph;
 
 import java.util.*;
 
-import static graph.WeightedGraph.*;
+import static graph.WeightedGraph.Node;
+import static java.lang.Integer.MAX_VALUE;
 
 /**
- * 그래프에서 두 정점 사이의 최단 거리를 찾기 위한 알고리즘
+ * Dijkstra Algorithm v2 (v1 리팩터링)
+ * - 그래프에서 두 정점 사이의 최단 거리를 찾기 위한 알고리즘
  * */
 public class DijkstraAlgorithm {
+    /**
+     * distance: 노드간 소요 거리를 저장할 객체, 노드를 지날 때마다 거리를 더함
+     * queue: 거리(가중치)가 짧은 노드를 먼저 실행하기 위한 우선순위 큐
+     * previous: 해당 노드에 도달하기 위한 최단 거리의 직전 노드를 가지는 객체
+     * g: 거리를 탐색할 가중치 그래프
+     * */
+    public final Map<String, Integer> distance = new HashMap<>();
+    public final Queue<Node> queue = new PriorityQueue<>(Comparator.comparingInt(o -> o.weight));
+    public final Map<String, String> previous = new HashMap<>();
+    public WeightedGraph g;
 
     public List<String> dijkstra(WeightedGraph g, String start, String end) {
-        /**
-         * 노드간 소요 거리를 저장할 객체
-         * 노드를 지날 때마다 거리를 더한다
-         * */
-        Map<String, Integer> distance = new HashMap<>();
+        this.g = g;
+        this.distance.put(start, 0);
+        this.queue.add(new Node(start, 0));
+        this.previous.put(start, "");
+        this.init(start);
+        this.pathSearch(end);
+        return pathGenerate(end);
+    }
 
-        // 거리(가중치)가 짧은 노드를 먼저 실행하기 위한 우선순위 큐
-        Queue<Node> queue = new PriorityQueue<>(Comparator.comparingInt(o -> o.weight));
+    /**
+     * 현재 까지 거리 + 다음 노드 까지 거리 < 기존 거리(초기값 = MAX_VALUE)
+     * - distance 객체에 거리 수정
+     * - previous 객체에 최단 거리 노드 수정
+     * - (우선순위)queue에 노드 추가
+     * */
+    private void pathSearch(String end) {
+        while(!this.queue.isEmpty()) {
+            String node = queue.poll().vertex;
 
-        // 해당 노드에 도달하기 위한 최단 거리의 직전 노드
-        Map<String, String> previous = new HashMap<>();
+            if (node.equals(end)) return;
 
-        // 초기 세팅
-        for (String key : g.adjacencyList.keySet()) {
-            int distanceValue = Integer.MAX_VALUE;
-            int weight = Integer.MAX_VALUE;
+            if (distance.getOrDefault(node, MAX_VALUE) == MAX_VALUE) continue;
 
-            if (key.equals(start)) {
-                distanceValue = 0;
-                weight = 0;
+            for (Node nextNode : g.adjacencyList.get(node)) {
+                // 현재 까지 거리 + 다음 노드 까지 거리
+                int candidate = distance.get(node) + nextNode.weight;
+                String nextVertex = nextNode.vertex;
+
+                if (candidate < distance.get(nextVertex)) {
+                    distance.put(nextVertex, candidate);
+                    previous.put(nextVertex, node);
+                    queue.add(new Node(nextVertex, candidate));
+                }
             }
-
-            // 시작 노드를 제외한 노드들의 거리와 가중치를 최대 값으로 설정
-            distance.put(key, distanceValue);
-            Node newNode = new Node(key, weight);
-            queue.add(newNode);
-            previous.put(key, "");
         }
-        // 반한 리스트
+    }
+
+    /**
+     * previous 마지막 노드부터 최단 거리 노드를 차례대로 stack에 입력
+     * stack을 사용해 path에 노드를 순서대로 입력할 수 있다.
+     * */
+    private List<String> pathGenerate(String smallest) {
+        Stack<String> stack = new Stack<>();
+
+        // 시작노드의 smallest는 빈 값이므로 시작노드에 도달하면 종료
+        while(!smallest.isEmpty()) {
+            stack.add(smallest);
+            String temp = smallest;
+            smallest = this.previous.get(smallest);
+            this.previous.remove(temp);
+        }
+
         List<String> path = new ArrayList<>();
 
-        while(!queue.isEmpty()) {
-            // 가중치가 가장 작은 노드(vertex)
-            String smallest = queue.poll().vertex;
-
-            // 가중치가 가장 작은 노드가 마지막 노드면
-            if (smallest.equals(end)) {
-                // node를 차례대로 출력하기 위해 값을 저장할 임시 stack
-                Stack<String> stack = new Stack<>();
-
-                /**
-                 * 시작 노드의 previous 값은 빈 값이며
-                 * previous의 마지막 노드부터 이전 노드(최단 거리 노드)를 차례대로 stack에 쌓는다
-                 * */
-                while(!smallest.isEmpty()) {
-                    stack.add(smallest);
-                    String temp = smallest;
-                    smallest = previous.get(smallest);
-                    previous.remove(temp);
-                }
-
-                // stack에 있는 노드를 path(반환 값)에 추가한다.
-                while(!stack.isEmpty()) {
-                    path.add(stack.pop());
-                }
-                return path;
-            }
-
-            // 가중치가 가장 짧은 노드가 있거나, 노드의 거리가 최대 값이 아닐 때
-            if (!smallest.isEmpty() || distance.get(smallest) != Integer.MAX_VALUE) {
-                for (Node nextNode : g.adjacencyList.get(smallest)) {
-                    // distance 객체에 있는 거리 값과 가중치를 더한 값
-                    int candidate = distance.get(smallest) + nextNode.weight;
-                    String nextVertex = nextNode.vertex;
-
-                    /**
-                     * 저장된 다음 노드로의 거리 보다 거리 합(candidate)이 더 작으면 값을 업데이트 한다.
-                     * 다음 노드로의 거리 최초 비교 값은 최대 값
-                     * */
-                    if (candidate < distance.get(nextVertex)) {
-                        distance.put(nextVertex, candidate);
-                        previous.put(nextVertex, smallest);
-                        queue.add(new Node(nextVertex, candidate));
-                    }
-                }
-            }
-
+        while(!stack.isEmpty()) {
+            path.add(stack.pop());
         }
 
         return path;
+    }
+
+    /**
+     * 초기값 세팅
+     * - 시작 노드를 제외한 노드들의 거리와 가중치를 최대 값으로 설정
+     * */
+    private void init(String start) {
+        for (String key : this.g.adjacencyList.keySet()) {
+            if (key.equals(start)) continue;
+
+            this.distance.put(key, MAX_VALUE);
+            this.queue.add(new Node(key, MAX_VALUE));
+            this.previous.put(key, "");
+        }
     }
 }
