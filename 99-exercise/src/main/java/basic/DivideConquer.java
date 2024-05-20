a87e4aee772941fc2277df22d5ac840466d89099
rtg <rtg1539@naver.com>
2024-05-20 10:52:18 +0900

    분할 정복 연습3

--- 99-exercise/src/main/java/basic/DivideConquer.java
+++ 99-exercise/src/main/java/basic/DivideConquer.java
@@ -85,4 +85,72 @@ public class DivideConquer {
         return e - s + 1;
     }
 
+    /**
+     * Method: findRotatedIndex
+     * Input
+     *  - 정렬된 숫자에서 중간에 로테이션 되는 배열, 정수
+     * Output
+     *  - 배열에 있는 정수의 위치(인덱스)
+     *  - 값을 찾을 수 없으면 -1을 반환
+     * Ex)
+     *  - findRotatedIndex([3,4,1,2],4) // 1
+     *  - findRotatedIndex([6, 7, 8, 9, 1, 2, 3, 4], 8) // 2
+     *  - findRotatedIndex([6, 7, 8, 9, 1, 2, 3, 4], 3) // 6
+     *  - findRotatedIndex([37,44,66,102,10,22],14) // -1
+     *  - findRotatedIndex([6, 7, 8, 9, 1, 2, 3, 4], 12) // -1
+     *  - findRotatedIndex([11,12,13,14,15,16,3,5,7,9], 16) // 5
+     *  - findRotatedIndex([11,12,13,0,1,2,3,5,7,9], 0) // 5
+     * Time Complexity - O(log n)
+     * Space Complexity - O(1)
+     * */
+    public static int findRotatedIndex(int[] arr, int num) {
+        /**
+         * 변수
+         *   - pivot: 로테이션 위치를 담기 위한 변수
+         *   - i: 로테이션 위치를 찾기 위해 위치를 1씩 더하거나 빼주기 위한 변수
+         * 로직
+         *  - 중간 값이 배열의 첫 번째 값보다 작으면
+         *    - 왼쪽 값 보다 작을 때까지 pivot의 위치를 왼쪽으로 이동
+         *  - 중간 값이 배열의 첫 번째 값보다 크면
+         *    - 오른쪽 값 보다 클 때까지 pivot의 위치를 오른쪽으로 이동
+         * */
+        int pivot = (arr.length - 1) / 2;
+        int i = arr[pivot] > arr[0] ? 1 : -1;
+
+        if (i < 0) {
+            while (true) {
+                if (arr[pivot] < arr[pivot + i]) break;
+
+                pivot += i;
+            }
+        } else {
+            while (true) {
+                if (arr[pivot] > arr[pivot + i]) break;
+
+                pivot += i;
+            }
+        }
+
+        /**
+         * start: 찾는 값이 배열의 첫 번째 값보다 크면 0 부터 시작, 아니면 pivot 위치부터 시작
+         * end: 찾는 값이 배열의 첫 번째 값보다 크면 pivot 까지 탐색, 아니면 마지막 인덱스 까지 탐색
+         * */
+        int start = num > arr[0] ? 0 : pivot;
+        int end = num > arr[0] ? pivot : arr.length - 1;
+
+        while (start <= end) {
+            int middle = (start + end) / 2;
+
+            if (num == arr[middle]) return middle;
+
+            if (num < arr[middle]) {
+                end = middle - 1;
+            } else {
+                start = middle + 1;
+            }
+        }
+
+        return -1;
+
+    }
 }
--- 99-exercise/src/test/java/basic/DivideConquerTest.java
+++ 99-exercise/src/test/java/basic/DivideConquerTest.java
@@ -5,6 +5,7 @@ import org.junit.jupiter.api.Test;
 import static org.assertj.core.api.Assertions.assertThat;
 import static basic.DivideConquer.countZeroes;
 import static basic.DivideConquer.sortedFrequency;
+import static basic.DivideConquer.findRotatedIndex;
 
 class DivideConquerTest {
     @Test
@@ -25,5 +26,17 @@ class DivideConquerTest {
         assertThat(sortedFrequency(new int[] {1,1,2,2,2,2,3},3)).isEqualTo(1);
         assertThat(sortedFrequency(new int[] {1,1,2,2,2,2,3},1)).isEqualTo(2);
         assertThat(sortedFrequency(new int[] {1,1,2,2,2,2,3},4)).isEqualTo(-1);
+    }
+
+
+    @Test
+    public void findRotatedIndexTest() {
+        assertThat(findRotatedIndex(new int[]{3,4,1,2},4)).isEqualTo(1);
+        assertThat(findRotatedIndex(new int[]{6, 7, 8, 9, 1, 2, 3, 4}, 8)).isEqualTo(2);
+        assertThat(findRotatedIndex(new int[]{6, 7, 8, 9, 1, 2, 3, 4}, 3)).isEqualTo(6);
+        assertThat(findRotatedIndex(new int[]{37,44,66,102,10,22},14)).isEqualTo(-1);
+        assertThat(findRotatedIndex(new int[]{6, 7, 8, 9, 1, 2, 3, 4}, 12)).isEqualTo(-1);
+        assertThat(findRotatedIndex(new int[]{11,12,13,14,15,16,3,5,7,9}, 16)).isEqualTo(5);
+        assertThat(findRotatedIndex(new int[]{11,12,13,0,1,2,3,5,7,9}, 0)).isEqualTo(3);
     }
 }
