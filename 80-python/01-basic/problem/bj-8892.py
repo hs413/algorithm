351946465f860704e006d9e32c1bac81ee9a556e
rtg <rtg1539@naver.com>
2024-10-25 19:13:40 +0900

    회문 추가 풀이(순열)

--- 80-python/01-basic/problem/bj-8892.py
+++ 80-python/01-basic/problem/bj-8892.py
@@ -39,13 +39,35 @@ for _ in range(T):
                 print(word1)
                 found = True
                 break
+                # exit(0)
             elif isPalindrome(word2):
                 print(word2)
                 found = True
                 break
+                # exit(0)
         
         if found:
             break
     
     if not found:
+        print(0)
+
+
+## 순열
+from itertools import permutations
+
+T = int(input())
+
+for _ in range(T):
+    k = int(input())
+    words = [input() for _ in range(k)]
+
+    for w1, w2 in permutations(words, 2):
+        p = w1 + w2
+
+        if p == p[::-1]:
+            print(p)
+            break
+    # 반복문이 정상 종료된 경우, break로 종료되지 않은 경우 실행
+    else:
         print(0)
