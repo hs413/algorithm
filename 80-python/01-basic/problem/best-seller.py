8eef50752bbc2bbfa7d1def1155e4f6bfe186da6
rtg <rtg1539@naver.com>
2024-10-25 19:13:59 +0900

    베스트셀러 추가 풀이

--- 80-python/01-basic/problem/best-seller.py
+++ 80-python/01-basic/problem/best-seller.py
@@ -8,3 +8,16 @@ dic = dict(Counter(list)) # 딕셔너리로 만들고 싶은 경우
 
 result = sorted(dic.items(), key=lambda x: (-x[1], x[0]))
 print(result[0][0])
+
+# 선형탐색 O(n)
+cnt = 0
+bestSeller = ''
+
+for bookName, selling in dic.items():
+    if selling > cnt:
+        cnt = selling
+        bestSeller = bookName
+    elif selling == cnt:
+        bestSeller = min(bestSeller, bookName)
+
+        
